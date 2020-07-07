d67cd286e08acec32209d1e67c02745c9789863a
Nimal <nimalgvwork@gmail.com>
2020-07-07 18:17:55 +0530

    login user done

--- modules/models.py
+++ modules/models.py
@@ -1,8 +1,10 @@
-from modules import db
+from modules import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, index=True)
     email = db.Column(db.String(100), unique=True, index=True)
@@ -11,6 +13,17 @@ class Users(db.Model):
 
     def __repr__(self):
         return f"<id : { self.id }>\n<username : { self.username }>\n<email : { self.email }>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
 
 
 class Posts(db.Model):
--- modules/routes.py
+++ modules/routes.py
@@ -1,17 +1,36 @@
 from modules import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from modules.forms import LoginForm
 from modules.variables import user, posts
+from modules.models import Users
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route("/")
+@login_required
 def home():
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested, user:{ form.username.data } and remeber me:{ form.remember_me.data }.")
-        return redirect(url_for("home"))
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Username or password is incorrect.")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page and url_parse(next_page).netloc != "":
+            return redirect(url_for("home"))
+        return redirect(next_page)
     return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
